f6efc9c226c723156ac8a1e43b3cf7798b374e1d
Cristian Carrillo <naitsircl@gmail.com>
2023-04-14 11:54:01 -0400

    avance Tarea1

---
+++ cl.bootcamp.abpro1/src/main/java/controladores/CrearContacto.java
@@ -0,0 +1,32 @@
+package controladores;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class CrearContacto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public CrearContacto() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/contacto.jsp");
+		rd.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ cl.bootcamp.abpro1/src/main/java/controladores/Inicio.java
@@ -0,0 +1,33 @@
+package controladores;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class Inicio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Inicio() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/inicio.jsp");
+		rd.forward(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
